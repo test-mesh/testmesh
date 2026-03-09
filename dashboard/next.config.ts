acbb79153271d9d39d8fb0ff114b3d142ac3a1c5
Georgi Georgiev <georgi.georgiev.it@gmail.com>
2026-03-09 11:14:45 +0200

    docker image workflow

--- dashboard/next.config.ts
+++ dashboard/next.config.ts
@@ -1,8 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  devIndicators: false
+  devIndicators: false,
+  output: 'standalone',
 };
 
 export default nextConfig;
