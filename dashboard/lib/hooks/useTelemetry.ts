6b7d51d2c9e0e4ea8f90cf5eed696599a2bd597f
Georgi Georgiev <georgi.georgiev.it@gmail.com>
2026-04-15 15:04:33 +0300

    fix(tracing): remove unused useQueryClient import from useTelemetry

--- dashboard/lib/hooks/useTelemetry.ts
+++ dashboard/lib/hooks/useTelemetry.ts
@@ -1,4 +1,4 @@
-import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import { useMutation, useQuery } from '@tanstack/react-query';
 import { telemetryApi } from '../api/client';
 
 // Query keys
